eb8c2c6964f40b2b53746d419b54bcdfee12c3c4
Garrett Seymour <nova4005@gmail.com>
2019-10-02 21:04:55 -0400

    start page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Boinc Headless Viewer</title>
+</head>
+<body>
+	<h1>Boinc Headless Viewer</h1>
+	<p>This web app will show you information about the boinc client running on your computer/server.</p>
+</body>
+</html>
